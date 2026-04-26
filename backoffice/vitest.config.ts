218c0fc484c86a26bedc9da0bbbbc119921b45fe
Duy Nguyễn <nhatduy123n@gmail.com>
2026-04-26 11:59:27 +0700

    fix coverage

--- backoffice/vitest.config.ts
+++ backoffice/vitest.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['**/*.test.ts', '**/*.test.tsx', '_test_utils/**/*.test.ts'],
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html', 'lcov'],  
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'json-summary', 'html', 'lcov'],  
       reportsDirectory: './coverage',
     },
   },
