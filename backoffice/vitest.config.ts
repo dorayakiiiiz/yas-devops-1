d849506d59053da9be86de5bf1a0d02ee9f7bc1b
Duy Nguyễn <nhatduy123n@gmail.com>
2026-04-26 15:54:33 +0700

    add common test

--- backoffice/vitest.config.ts
+++ backoffice/vitest.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@commonServices': path.resolve(__dirname, './common/services'),
-    },
-  },
   test: {
     environment: 'jsdom',
     include: [
